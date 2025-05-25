04d536b43a4d14725fed97e2c838b426fd2d7049
Ofir-Coleague <ofir@coleague.co.il>
2025-05-26 01:27:38 +0300

    Update index.js

--- linkedin-gpt-server/index.js
+++ linkedin-gpt-server/index.js
@@ -4,15 +4,16 @@ const { chromium } = require('playwright');
 async function scrapeProfileData(profileUrl) {
   const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
-  await context.addCookies([{
-    name: 'li_at',
-    value: process.env.LI_AT,
-    domain: '.linkedin.com',
-    path: '/',
-    httpOnly: true,
-    secure: true,
-    sameSite: 'Lax'
-  }]);
+await context.addCookies([{
+  name: 'li_at',
+  value: 'AQEFAQ4BAAAAABS9zB0AAAGVybCQ5wAAAZcn--zRTQAAsXVybjpsaTplbnRlcnByaXNlQXV0aFRva2VuOmVKeGpaQUFDbHNlN00wRTBXNWU0SUlobUxhbGF5UWhpYUlrNnhJSVozTGF4a2d5TUFKWmFCZjQ9XnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjgyMDMzNTEzLDEwOTcxMzE2OSledXJuOmxpOm1lbWJlcjo0OTI5NjU5NherHqrIEOcuIj_WSd4A5M9erC2N4Cb3fWZPgQZIECO85KTkzWwSjAYTHp2O7ln0nX4xPUL4WF5VoGmY5Q2uq9a35w4b7amUbXq550wWULGoeDxG-7lrudF-HbeTgJQ78vrnYXlowZTPciqP1KdBZMuZWlXOkjsfxmf2mGZLgTaZr113br637Wocn7cdMbr8Z2_D_n8', // ← כאן
+  domain: '.linkedin.com',
+  path: '/',
+  httpOnly: true,
+  secure: true,
+  sameSite: 'Lax'
+}]);
+
 
   const page = await context.newPage();
   await page.goto(profileUrl, { waitUntil: 'domcontentloaded' });
