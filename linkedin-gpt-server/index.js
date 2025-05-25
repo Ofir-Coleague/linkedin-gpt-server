46197d5469b204c4d672ffd79fe5ecc0351fd4a2
Ofir-Coleague <ofir@coleague.co.il>
2025-05-26 01:44:48 +0300

    Update index.js

--- linkedin-gpt-server/index.js
+++ linkedin-gpt-server/index.js
@@ -6,7 +6,7 @@ async function scrapeProfileData(profileUrl) {
   const context = await browser.newContext();
 await context.addCookies([{
   name: 'li_at',
-  value: 'AQEFAQ4BAAAAABS9zB0AAAGVybCQ5wAAAZcn--zRTQAAsXVybjpsaTplbnRlcnByaXNlQXV0aFRva2VuOmVKeGpaQUFDbHNlN00wRTBXNWU0SUlobUxhbGF5UWhpYUlrNnhJSVozTGF4a2d5TUFKWmFCZjQ9XnVybjpsaTplbnRlcnByaXNlUHJvZmlsZToodXJuOmxpOmVudGVycHJpc2VBY2NvdW50OjgyMDMzNTEzLDEwOTcxMzE2OSledXJuOmxpOm1lbWJlcjo0OTI5NjU5NherHqrIEOcuIj_WSd4A5M9erC2N4Cb3fWZPgQZIECO85KTkzWwSjAYTHp2O7ln0nX4xPUL4WF5VoGmY5Q2uq9a35w4b7amUbXq550wWULGoeDxG-7lrudF-HbeTgJQ78vrnYXlowZTPciqP1KdBZMuZWlXOkjsfxmf2mGZLgTaZr113br637Wocn7cdMbr8Z2_D_n8', // ← כאן
+  value: 'AQEDAQLwNNQCtVvHAAABlwmWzqsAAAGXLaNSq00AZfuPKdo7x8uAo_3gKaKdb5jMBBbNMzXChjgVyAj7D8WIERcNtHwuwKH9-2hv4Ycz92r31fa0JBrXSbh9i0bxJqrPSJnssOmtkj1YmWeYIUMXaPQw', // ← כאן
   domain: '.linkedin.com',
   path: '/',
   httpOnly: true,
